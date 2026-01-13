73ad8a5cbf26d1b50266f9b059d96c4db0472bef
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-13 21:20:27 +0300

    update airplane field

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,7 +1,11 @@
 from .models import Flight
 from rest_framework.serializers import ModelSerializer
+from airplanes.serializers import AirplaneSerializer
 
 class FlightSerializer(ModelSerializer):
+    
+    airplane = AirplaneSerializer()
+
     class Meta:
         model = Flight
         fields = "__all__"
