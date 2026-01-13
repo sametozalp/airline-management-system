7e647d435c8b34e633a40d1251e899b09c9c8c53
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-13 18:43:04 +0300

    add get all flights

--- flights/views.py
+++ flights/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from.models import Flight
+from django.http import JsonResponse
 
 # Create your views here.
+
+def get_all(req):
+    flights = Flight.objects.all()
+    data = {
+        'flights': list(flights.values())
+    }
+
+    return JsonResponse(data)
