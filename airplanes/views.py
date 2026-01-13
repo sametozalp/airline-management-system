e890fbc28553b7af4bad0902b779744c89be38c1
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-13 20:37:18 +0300

    add get_all for airplanes

--- airplanes/views.py
+++ airplanes/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Airplane
+from django.http import JsonResponse
 
 # Create your views here.
+
+def get_all(req):
+    airplanes = Airplane.objects.all()
+    data = {
+        'airplanes': list(airplanes.values())
+    }
+    return JsonResponse(data)
--- core/urls.py
+++ core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('flights/', include('flights.urls'))
+    path('flights/', include('flights.urls')),
+    path("airplanes/", include('airplanes.urls'))
 ]
