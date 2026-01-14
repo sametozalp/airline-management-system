3461f7c86296518c40ac3c7a7a5cc862f781a590
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-14 13:29:55 +0300

    update urls

--- flights/urls.py
+++ flights/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import get_all, get_detail
 
 urlpatterns = [
-    path('get_all', get_all, name='get_all'),
+    path('', get_all, name='get_all'),
     path('<int:id>', get_detail, name='get_detail')
 ]
--- reservations/serializers.py
+++ reservations/serializers.py
@@ -1,12 +1,12 @@
 from rest_framework.serializers import ModelSerializer
 from .models import Reservation
 
-class ReservationBasicSerializer(ModelSerializer):
+class ReservationDetailSerializer(ModelSerializer):
     class Meta:
         model = Reservation
         fields = "__all__"
 
-class ReservationDetailSerializer(ModelSerializer):
+class ReservationBasicSerializer(ModelSerializer):
     class Meta:
         model = Reservation
         fields = ["id", "passenger_name", "reservation_code", "flight"]
