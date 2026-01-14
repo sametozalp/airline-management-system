7af7784398879224155f66c9b760472b6c47417c
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-14 19:43:51 +0300

    update reservation views

--- reservations/reservation_views/reservation_update_detail_view.py
+++ reservations/reservation_views/reservation_update_detail_view.py
@@ -1,29 +1,15 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
-from .models import Reservation
+from ..models import Reservation
 from rest_framework.response import Response
-from .serializers.reservation_basic_serializer import ReservationBasicSerializer
-from .serializers.reservation_detail_serializer import ReservationDetailSerializer
-from .serializers.reservation_create_serializer import ReservationCreateSerializer
+from ..serializers.reservation_basic_serializer import ReservationBasicSerializer
+from ..serializers.reservation_detail_serializer import ReservationDetailSerializer
+from ..serializers.reservation_create_serializer import ReservationCreateSerializer
+from ..serializers.reservation_update_serializer import ReservationUpdateSerializer
 from rest_framework import status
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404
 
-# Create your views here.
-
-class ReservationListCreateView(APIView):
-    
-    def get(self, req):
-        reservartions = Reservation.objects.all()
-        serializer = ReservationBasicSerializer(reservartions, many=True)
-        return Response(serializer.data)
-    
-    def post(self, req):
-        serializer = ReservationCreateSerializer(data=req.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response (serializer.data, status=status.HTTP_201_CREATED)
-        
 class ReservationUpdateDetailView(APIView):
 
     def get(self, req, id):
--- reservations/urls.py
+++ reservations/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import ReservationUpdateDetailView, ReservationListCreateView
+# from .views import ReservationUpdateDetailView, ReservationListCreateView
+from .reservation_views.reservation_list_create_view import ReservationListCreateView
+from .reservation_views.reservation_update_detail_view import ReservationUpdateDetailView
 
 urlpatterns = [
     path('', ReservationListCreateView.as_view(), name='reservation-list-create'),
     path('<int:id>', ReservationUpdateDetailView.as_view(), name='reservation-detail-update')
 ]
+
