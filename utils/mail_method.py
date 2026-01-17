0e48c497ed6d6ce9f95872979c87b8b2076ce3c9
Samet Özalp <93615071+sametozalp@users.noreply.github.com>
2026-01-18 00:41:52 +0300

    update mail method

--- utils/mail_method.py
+++ utils/mail_method.py
@@ -3,12 +3,16 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 def send_email(subject, message, to):
-    recipient_list = [to]
+    try:      
+        recipient_list = [to]
 
-    send_mail(
-        subject,
-        message,
-        settings.DEFAULT_FROM_EMAIL,
-        recipient_list,
-        fail_silently=True,
-    )
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            recipient_list,
+            fail_silently=True,
+        )
+
+    except Exception as e:
+        print(e)
